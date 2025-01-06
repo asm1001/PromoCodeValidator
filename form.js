90f3c228d9f60733d2970028ad57f7d1da01e634
Andres Trevino <andres.a.trevino@protonmail.com>
2025-01-06 09:55:07 -0600

    solved

--- form.js
+++ form.js
@@ -1,4 +1,13 @@
 export function isPromoCodeValid(code) {
-    console.log(code)
-    
+  console.log(code);
+
+  code = code.trim();
+
+  if (code.length < 5) {
+    return false;
+  } else if (code.length > 10) {
+    return false;
+  } else {
+    return true;
+  }
 }
